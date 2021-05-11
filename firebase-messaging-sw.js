6bfbe337de23ce73f50da3776101e579209630b1
Bruno Silva <bruno3dcontato@gmail.com>
2021-05-11 14:32:24 -0300

    Create firebase-messaging-sw.js
    feat: added service worker structure

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,91 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-console */
+/* global importScripts, firebase */
+importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-messaging.js');
+
+console.log('===========================================');
+console.log('FIREBASE SERVICE WORKER CARREGADO'); // debug info
+console.log('===========================================');
+
+firebase.initializeApp({
+  apiKey: '',
+  projectId: '',
+  messagingSenderId: '',
+  appId: '',
+  authDomain: '',
+  databaseURL: '',
+  storageBucket: '',
+  measurementId: '',
+});
+
+class CustomPushEvent extends Event {
+  constructor(data) {
+    super('push');
+
+    Object.assign(this, data);
+    this.custom = true;
+  }
+}
+
+/*
+ * Overrides push notification data, to avoid having 'notification' key and firebase blocking
+ * the message handler from being called
+ */
+self.addEventListener('push', (e) => {
+  // Skip if event is our own custom event
+  if (e.custom) return;
+
+  // Kep old event data to override
+  const oldData = e.data;
+
+  // Create a new event to dispatch, pull values from notification key and put it in data key,
+  // and then remove notification key
+  const newEvent = new CustomPushEvent({
+    data: {
+      ehheh: oldData.json(),
+      json() {
+        const newData = oldData.json();
+        newData.data = {
+          ...newData.data,
+          ...newData.notification,
+        };
+        delete newData.notification;
+        return newData;
+      },
+    },
+    waitUntil: e.waitUntil.bind(e),
+  });
+
+  // Stop event propagation
+  e.stopImmediatePropagation();
+
+  // Dispatch the new wrapped event
+  dispatchEvent(newEvent);
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  // console.log('[firebase-messaging-sw.js] Received background message ', payload); // debug info
+
+  const { title, body, icon, ...restPayload } = payload.data;
+
+  const notificationOptions = {
+    body,
+    icon: icon || '/icons/firebase-logo.png', // path to your "fallback" firebase notification logo
+    data: restPayload,
+  };
+
+  return self.registration.showNotification(title, notificationOptions);
+});
+
+self.addEventListener('notificationclick', (event) => {
+  // console.log('[firebase-messaging-sw.js] notificationclick ', event); // debug info
+
+  self.clients.openWindow(
+    event.notification.click_action || event.currentTarget.origin,
+  );
+  // close notification after click
+  event.notification.close();
+});
